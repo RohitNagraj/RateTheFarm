6bd2fac0b33f99a51e7fc95389878cd135aa3023
DESKTOP-5PGTGEF\rohit <rohitnagraj.99@gmail.com>
2018-08-06 12:52:26 +0530

    Updated the UserData class to hold all the farmer variables, completed half of the data fetching page.

--- app/src/main/java/com/example/msrit/ratethefarm/NeeharGetFarmerDetails.java
+++ app/src/main/java/com/example/msrit/ratethefarm/NeeharGetFarmerDetails.java
@@ -8,142 +8,128 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class NeeharGetFarmerDetails extends AppCompatActivity {
-    EditText mname;
-    EditText mvillage;
-    EditText mhobli;
-    EditText mtaluq;
-    EditText mage;
 
-    TextView labour_view;
-    EditText mmale;
-    EditText mfemale;
-
-    TextView land_view;
-    EditText mtotal_land;
-
-    RadioGroup rg_landtype;
-    RadioButton rbIrrigated;
-    RadioButton rbRainfed;
-
-    TextView animal_view;
-    EditText mcow,mbuffalo,mcock,mhen,msheep,mgoat,mothers;
-
-    TextView sericulture_view,extent_view;
-    EditText mextent;
-
-    RadioGroup rg_mulberry;
-    RadioButton rb_yes_mul;
-    RadioButton rb_no_mul;
-
-    TextView machinery_view, owned_view;
-    CheckBox ctractor,cpower_tiller,cplougher,crotomator,cbullock,csprayer,csprinkler;
-
-    TextView crop_view,name_view,cropextent_view,production_view,misc_view,online_view,nursery_view,sales_view;
-    EditText mcrop_name,mcrop_extent,mcrop_prod;
-
-    RadioGroup rg_online;
-    RadioButton rb_yes_online;
-    RadioButton rb_no_online;
-
-    RadioGroup rg_scientific;
-    RadioButton rb_yes_sci;
-    RadioButton rb_no_sci;
-
-    RadioGroup rg_nursery;
-    RadioButton rb_yes_nursery;
-    RadioButton rb_no_nursery;
-
-    CheckBox clocal,capmc;
-
-    Button submit;
+    UserData userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neehar_get_farmer_details);
 
-        mname = (EditText) findViewById(R.id.name);
-        mvillage = (EditText) findViewById(R.id.village);
-        mhobli = (EditText) findViewById(R.id.hobli);
-        mtaluq = (EditText) findViewById(R.id.taluq);
-        mage = (EditText) findViewById(R.id.age);
-        mmale = (EditText) findViewById(R.id.male);
-        mfemale = (EditText) findViewById(R.id.female);
+        //Personal Details
 
-        mtotal_land = (EditText) findViewById(R.id.total_land);
-        rg_landtype = (RadioGroup) findViewById(R.id.i_or_r);
-        rbIrrigated = (RadioButton) findViewById(R.id.irrigated);
-        rbRainfed = (RadioButton) findViewById(R.id.rainfed);
+        final EditText mName = findViewById(R.id.name);
+        final EditText mVillage = findViewById(R.id.village);
+        final EditText mHobli = findViewById(R.id.hobli);
+        final EditText mTaluq = findViewById(R.id.taluq);
+        final EditText mAge = findViewById(R.id.age);
 
-        mcow = (EditText) findViewById(R.id.cow);
-        mbuffalo = (EditText) findViewById(R.id.buffalo);
-        mcock = (EditText) findViewById(R.id.cock);
-        mhen = (EditText) findViewById(R.id.hen);
-        msheep = (EditText) findViewById(R.id.sheep);
-        mgoat = (EditText) findViewById(R.id.goat);
-        mothers = (EditText) findViewById(R.id.others);
-        mextent = (EditText) findViewById(R.id.extent);
+        //Total no. of Workers
 
-        rg_mulberry = (RadioGroup) findViewById(R.id.yn_mulberry);
-        rb_yes_mul = (RadioButton) findViewById(R.id.yes_mul);
-        rb_no_mul = (RadioButton) findViewById(R.id.no_mul);
+        final EditText mMaleWorkers = findViewById(R.id.workers_men);
+        final EditText mFemaleWorkers = findViewById(R.id.workers_women);
 
-        ctractor = (CheckBox) findViewById(R.id.tractor);
-        cpower_tiller = (CheckBox) findViewById(R.id.power_tiller);
-        cplougher = (CheckBox) findViewById(R.id.plougher);
-        crotomator = (CheckBox) findViewById(R.id.rotomator);
-        cbullock = (CheckBox) findViewById(R.id.bullock_cart);
-        csprayer = (CheckBox) findViewById(R.id.sprayer);
-        csprinkler = (CheckBox) findViewById(R.id.sprinkler);
+        //Land Details
 
-        mcrop_name = (EditText) findViewById(R.id.crop_name);
-        String  crop_name = mcrop_name.getText().toString();
+        final EditText mLandArea = findViewById(R.id.land_area);
+        final RadioGroup rgLandType = findViewById(R.id.land_type);
 
-        mcrop_extent = (EditText) findViewById(R.id.crop_extent);
-        String  crop_extent = mcrop_extent.getText().toString();
+        //Animals Owned
 
-        mcrop_prod = (EditText) findViewById(R.id.crop_production);
-        String  crop_production = mcrop_prod.getText().toString();
+        final EditText mCow = findViewById(R.id.cow);
+        final EditText mBuffalo = findViewById(R.id.buffalo);
+        final EditText mCock = findViewById(R.id.cock);
+        final EditText mHen = findViewById(R.id.hen);
+        final EditText mSheep = findViewById(R.id.sheep);
+        final EditText mGoat = findViewById(R.id.goat);
+        final EditText mOtherAnimals = findViewById(R.id.others);
 
-        rg_online = (RadioGroup) findViewById(R.id.yn_online);
-        rb_yes_online = (RadioButton) findViewById(R.id.yes_online);
-        rb_no_online = (RadioButton) findViewById(R.id.no_online);
+        //Sericulture
 
-        rg_scientific = (RadioGroup) findViewById(R.id.yn_scientific);
-        rb_yes_sci = (RadioButton) findViewById(R.id.yes_scientific);
-        rb_no_sci = (RadioButton) findViewById(R.id.no_scientific);
+        final EditText mMulberryYield = findViewById(R.id.mulberry_yield);
+        final RadioGroup rgMulberry = findViewById(R.id.sell_mulberry);
 
-        rg_nursery = (RadioGroup) findViewById(R.id.yn_nursery);
-        rb_yes_nursery = (RadioButton) findViewById(R.id.yes_nursery);
-        rb_no_nursery = (RadioButton) findViewById(R.id.no_nursery);
+        //Machines Owned
 
-        clocal = (CheckBox) findViewById(R.id.sale_local);
-        capmc = (CheckBox) findViewById(R.id.sale_apmc);
+        CheckBox cTractor = findViewById(R.id.tractor);
+        CheckBox cPowerTiller = findViewById(R.id.power_tiller);
+        CheckBox cPlougher = findViewById(R.id.plougher);
+        CheckBox cRotomator = findViewById(R.id.rotomator);
+        CheckBox cBullock = findViewById(R.id.bullock_cart);
+        CheckBox cSprayer = findViewById(R.id.sprayer);
+        CheckBox cSprinkler = findViewById(R.id.sprinkler);
 
-        submit = (Button) findViewById(R.id.submit);
+        //Crop Details
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        EditText mCrop = findViewById(R.id.crop_name);
+        EditText mCropArea = findViewById(R.id.crop_area);
+        EditText mCropYield = findViewById(R.id.crop_yield);
+
+        //Miscellaneous
+
+        RadioGroup rgOnlineSales = findViewById(R.id.sell_online);
+        RadioButton  rbOnlineYes = findViewById(R.id.online_yes);
+        RadioButton  rbOnlineNo = findViewById(R.id.online_no);
+
+        RadioGroup rgScientificSuggestions = findViewById(R.id.scientific_suggestions);
+        RadioButton rbYesScience = findViewById(R.id.scientific_yes);
+        RadioButton rbNoScience = findViewById(R.id.scientific_no);
+
+        RadioGroup rgNursery = findViewById(R.id.own_nursery);
+        RadioButton rbYesNursery = findViewById(R.id.nursery_yes);
+        RadioButton rbNoNursery = findViewById(R.id.nursery_no);
+
+        CheckBox cLocal = findViewById(R.id.sale_local);
+        CheckBox cAMPC = findViewById(R.id.sale_apmc);
+
+        Button mSubmitBtn = findViewById(R.id.submit);
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = mname.getText().toString();
-                String village = mvillage.getText().toString();
-                String hobli = mhobli.getText().toString();
-                String taluq = mtaluq.getText().toString();
-                int age = Integer.parseInt(mage.getText().toString());
-                int male = Integer.parseInt(mmale.getText().toString());
-                int female = Integer.parseInt(mfemale.getText().toString());
-                int total_land = Integer.parseInt(mtotal_land.getText().toString());
-                int cow = Integer.parseInt(mcow.getText().toString());
-                int buffalo = Integer.parseInt(mbuffalo.getText().toString());
-                int cock = Integer.parseInt(mcock.getText().toString());
-                int hen = Integer.parseInt(mhen.getText().toString());
-                int sheep = Integer.parseInt(msheep.getText().toString());
-                int goat = Integer.parseInt(mgoat.getText().toString());
-                int others = Integer.parseInt(mothers.getText().toString());
-                int extent = Integer.parseInt(mextent.getText().toString());
+
+                userData.setName(mName.getText().toString());
+                userData.setVillage(mVillage.getText().toString());
+                userData.setHobli(mHobli.getText().toString());
+                userData.setTaluq(mTaluq.getText().toString());
+                userData.setAge(Integer.parseInt(mAge.getText().toString()));
+
+
+                userData.setMaleWorkers(Integer.parseInt(mMaleWorkers.getText().toString()));
+                userData.setFemaleWorkers(Integer.parseInt(mFemaleWorkers.getText().toString()));
+
+
+                userData.setLand(Double.parseDouble(mLandArea.getText().toString()));
+                int SelectedID = rgLandType.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(SelectedID);
+                userData.setIrrigatedOrRainfed(radioButton.getText().toString());
+
+
+                userData.setCow(Integer.parseInt(mCow.getText().toString()));
+                userData.setBuffalo(Integer.parseInt(mBuffalo.getText().toString()));
+                userData.setCock(Integer.parseInt(mCock.getText().toString()));
+                userData.setHen(Integer.parseInt(mHen.getText().toString()));
+                userData.setSheep(Integer.parseInt(mSheep.getText().toString()));
+                userData.setGoat(Integer.parseInt(mGoat.getText().toString()));
+                userData.setOtherAnimals(Integer.parseInt(mOtherAnimals.getText().toString()));
+
+
+                userData.setMulberryYield(Double.parseDouble(mMulberryYield.getText().toString()));
+                SelectedID = rgMulberry.getCheckedRadioButtonId();
+                radioButton = findViewById(SelectedID);
+                String SellMulberry = radioButton.getText().toString();
+                if (SellMulberry.equals("Yes"))
+                {
+                    userData.setSellMulberry(true);
+                }
+                else
+                {
+                    userData.setSellMulberry(false);
+                }
+
+
                 }
         });
 
