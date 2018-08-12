4a12e4a2b93cdb203af0f81f5266aaead11fd8d3
DESKTOP-5PGTGEF\rohit <rohitnagraj.99@gmail.com>
2018-08-12 21:28:38 +0530

    Added ShowFarmerDetails example

--- app/src/main/java/com/example/msrit/ratethefarm/ShowFarmerDetails.java
+++ app/src/main/java/com/example/msrit/ratethefarm/ShowFarmerDetails.java
@@ -1,8 +1,11 @@
 package com.example.msrit.ratethefarm;
 
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +18,9 @@ public class ShowFarmerDetails extends AppCompatActivity {
 
     DatabaseReference mDatabase;
 
+    private TextView mName;
+    private TextView mAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +28,23 @@ public class ShowFarmerDetails extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("Users").child(MyAdapter.mClickID).addValueEventListener(new ValueEventListener() {
+            @SuppressLint("WrongViewCast")
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 UserData value = dataSnapshot.getValue(UserData.class);
                 Toast.makeText(ShowFarmerDetails.this, value.getName(), Toast.LENGTH_SHORT).show();
+
+                //Enter Code Here
+
+                mName = findViewById(R.id.textView);
+                mName.setText(value.getName());
+
+                mAge = findViewById(R.id.textView2);
+                mAge.setText(Integer.toString(value.getAge()));
+
+
+
+
             }
 
 
