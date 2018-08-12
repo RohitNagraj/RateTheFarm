034588d8b0bec36fd73f47de17a46b3fba20b3fb
Shubhang Kaushik <33983863+ShubhangPK@users.noreply.github.com>
2018-08-12 22:40:24 +0530

    Update ShowFarmerDetails.java

--- app/src/main/java/com/example/msrit/ratethefarm/ShowFarmerDetails.java
+++ app/src/main/java/com/example/msrit/ratethefarm/ShowFarmerDetails.java
@@ -1,11 +1,8 @@
 package com.example.msrit.ratethefarm;
 
-import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,50 +10,44 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Button;
+import android.widget.Toast;
 public class ShowFarmerDetails extends AppCompatActivity {
+UserData man = new UserData();
     DatabaseReference mDatabase;
-
-    private TextView mName;
-    private TextView mAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_farmer_details);
-           
+
+        
         final RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.simpleRatingBar);
         Button submitButton = (Button) findViewById(R.id.submitButton);
-       
+        
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+                
                 String totalStars = "Total Stars:: " + simpleRatingBar.getNumStars();
                 String rating = "Rating :: " + man.getRating();
                 Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
+                
+                
+                
+                
             }
         });
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mDatabase.child("Users").child(MyAdapter.mClickID).addValueEventListener(new ValueEventListener() {
-            @SuppressLint("WrongViewCast")
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 UserData value = dataSnapshot.getValue(UserData.class);
                 Toast.makeText(ShowFarmerDetails.this, value.getName(), Toast.LENGTH_SHORT).show();
-
-                //Enter Code Here
-
-                mName = findViewById(R.id.textView);
-                mName.setText(value.getName());
-
-                mAge = findViewById(R.id.textView2);
-                mAge.setText(Integer.toString(value.getAge()));
-
-
-
-
             }
 
 
